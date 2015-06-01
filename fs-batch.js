d7cafd5eee13fb404442bafdde95e1af5f2a630a
Sam Eaton <sameaton11@gmail.com>
2015-06-01 16:42:22 -0600

    adds the fsb and helpers objects with respective methods

--- fs-batch.js
+++ fs-batch.js
@@ -5,8 +5,27 @@ var helpers = {
 		return (typeof _INPUT === "string") ? "string" : ( (Array.isArray(_INPUT)) ? "array" : null ); 
 	},
 
-	getMultiFiles: function(ARR){
-		
+	prepareQuery: function(_PATH, _ACTION){
+		var self = helpers;
+		var newFileQuery = {
+			path: _PATH,
+			action: _ACTION,
+			pending: true,
+			send: null,
+			receive: null
+		};
+
+		if(newFileQuery.action === "write"){
+			newFileQuery.send = _SEND_DATA;
+		}
+		self.queue.push(newFileQuery);
+	},
+	getMultiFiles: function(_ARR, _ACTION){
+		var self = helpers;
+		for(var i = 0, ii = _ARR.length; i < ii; i++){
+			self.prepareQuery(_ARR[i], _ACTION);
+		}
+		self.readQueue(); //* left here
 	},
 
 	dataReturned: function(){
@@ -14,24 +33,74 @@ var helpers = {
 			for(var t=this,e=0,n=t.length;n>e;e++)s(t[e],e,t)
 		};
 	},
+	readQueue: function(){
+		var self = helpers;
+		for (var i = 0, ii = self.queue.length; i<ii; i++) {
+			if(self.queue[i] !== null){
+				self.readFile(self.queue[i], self.queue, i);
+			} 
+		}
+	},
+	readFile: function(_FILE_OBJ, _FILE_QUEUE_ARRAY, _INDEX){
+		var self = helpers;
+		// console.log(_FILE_OBJ);
+		var fileObject = {
+			query: _FILE_OBJ.path
+		};
+		fs.readFile(_FILE_OBJ.path, 'utf8',function(err, data){
+			if(err) {
+				fileObject.error = err;
+				self.dataAll.push(fileObject);
+				self.dataError.push(fileObject);
+				// console.log("err: ", err);
+			} else {
+				fileObject.data = data;
+				self.dataAll.push(fileObject);
+				self.dataSuccess.push(fileObject);
+				// console.log("data: ", data)
+			}
+			
+			console.log("self.dataSuccess.length:\t",self.dataSuccess.length);
+			console.log("self.dataError.length:    \t",self.dataError.length);
+			console.log("");
+			console.log("self.dataAll.length:    \t",self.dataAll.length);
+			console.log("self.queue.length:      \t",self.queue.length);
+			if(self.queue.length === self.dataAll.length){
+				console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>\tMATCH")
+			}
+			console.log("");
+			console.log("-----------------------------");
 
+			// self.dataAll.push()
+		})
+	},
+	fileQuery: function(){
+
+	},
 	queue: [],
 	dataAll: [],
+	dataSuccess: [],
 	dataError: []
 };
 
 
 var fsb = {
 
-	read: function(fd, buffer, offset, length, position, callback){
+	readFiles: function(fd){
 		switch(helpers.inputType(fd)){
-			case "string":
-				console.log("string mofo");
-				break;
 			case "array":
-				getMultiFiles(fd);
+				helpers.getMultiFiles(fd, "read");
+				break;
+			case "string":
+				console.log("fs-batch: string != array of strings");
+				return;
+				break;
+			default:
+				console.log("fs-batch: must pass array of strings");
+				return;
 				break;
 		}
+
 		return fsb;
 	},
 
@@ -42,7 +111,7 @@ var fsb = {
 
 	utils: {
 		onSuccess: function(){},
-		onError: function(){},
+		onError: function(){}
 	}
 };
 
--- index.js
+++ index.js
@@ -21,68 +21,11 @@ server.on("request", function(req,res){
 server.listen(8000);
 
 
-// http.createServer(function(req,res){
-// 	// console.log("req: ",req);
-// 	console.log(" ");
-// 	console.log(" ");
-// 	console.log("======================");
-// 	console.log("======================");
-// 	console.log("req.headers > ",req.headers);
-// 	console.log("--------------");
-// 	console.log("req.headers.host > ",req.headers.host);
-// 	console.log("--------------");
-// 	console.log("req.headers['accept-encoding'] > ",req.headers["accept-encoding"]);
-// 	console.log("--------------");
-// 	console.log("req.rawHeaders > ",req.rawHeaders);
-// 	console.log("--------------");
-// 	console.log("req.url > ",req.url);
-// 	console.log("--------------");
-// 	console.log("req.method >",req.method);
-// 	console.log("======================");
-// 	console.log("======================");
-
-// 	console.log(" ");
-// 	console.log(" ");
-// 	console.log(" ");
-
-// 	console.log("^^^^^^^^^^^^^^^^^^^^^^^^");
-// 	console.log("^^^^^^^ RESPONSE ^^^^^^^");
-// 	console.log("^^^^^^^^^^^^^^^^^^^^^^^^");
-// 	// console.log("res : ",res);
-// 	// console.log("--------------");
-// 	// console.log("req.headers.host > ",req.headers.host);
-// 	// console.log("--------------");
-// 	// console.log("req.headers['accept-encoding'] > ",req.headers["accept-encoding"]);
-// 	// console.log("--------------");
-// 	// console.log("req.rawHeaders > ",req.rawHeaders);
-// 	// console.log("--------------");
-// 	// console.log("req.url > ",req.url);
-// 	// console.log("--------------");
-// 	// console.log("req.method >",req.method);
-// 	console.log("======================");
-// 	console.log("======================");
-
-
-// 	res.writeHeader(200, {"Content-Type": "text/plain"});
-// 	res.write("Welcome to Node.js, SamR");
-// 	res.end();
-// 	console.log("res : ",res);
-// 	console.log("======================");
-// 	console.log("======================");
-// 	// res.end();
-// }).listen(8000);
-
-// console.log("Server running on port :8000");
 var fsb = require("./fs-batch");
 
-// console.log("output: ",kb.add(9,2));
-// console.log(path.join(__dirname, 'poopyhead'));
-// console.error("error msg");
-// console.warn("warning msg");
-fsb().read("kitty")
-.success(function(files){
-	files.each(function(file){
-		console.log(file);
-	})
-});
-// console.log(fsb());
+fsb().readFiles(["cats.json", "dogs.json", "pigs.json"])
+// .success(function(files){
+// 	files.each(function(file){
+// 		console.log(file);
+// 	})
+// });
